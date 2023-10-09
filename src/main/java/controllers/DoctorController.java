1152e1497807071cc3b587215b401f270df1cf16
clauga <guerrero.ayala.cs@gmail.com>
2023-10-09 16:42:19 +0200

    clean

--- src/main/java/controllers/DoctorController.java
+++ src/main/java/controllers/DoctorController.java
@@ -10,20 +10,19 @@ public class DoctorController {
 
     @GetMapping("/doctor/1")
     @ResponseBody
-    public String index() {
+    public String doctor1() {
         return "William Hartnell";
     }
 
     @GetMapping("/doctor/10")
     @ResponseBody
-    public String index10() {
+    public String doctor10() {
         return "David Tennant";
     }
 
     @GetMapping("/doctor/13")
     @ResponseBody
-    public String index13() {
+    public String doctor13() {
         return "Jodie Whittaker";
     }
-
 }
