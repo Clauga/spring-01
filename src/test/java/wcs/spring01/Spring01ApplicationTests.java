2696f7cbc4223efe903c29745e160ac25f4786b1
clauga <guerrero.ayala.cs@gmail.com>
2023-10-09 16:40:15 +0200

    Initial commit

---
+++ src/test/java/wcs/spring01/Spring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package wcs.spring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
